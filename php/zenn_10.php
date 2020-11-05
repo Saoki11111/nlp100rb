c69a84400bc1e7ef181335040101c80e527b9ab9
Saoki11111 <t.aoki11111@gmail.com>
2020-11-05 22:09:29 +0900

    最初のユーザー?

---
+++ php/zenn_10.php
@@ -0,0 +1,17 @@
+<?php
+# 以下の連想配列(users)の中から、
+# 管理者権限(admin)を持っている(true)ユーザーを探し、
+# 最初に見つけた(true)ユーザーをadminという変数に格納して下さい。
+
+$users = [
+  [ "id" => 1, "admin" => "false" ],
+  [ "id" => 2, "admin" => "true" ],
+  [ "id" => 3, "admin" => "false" ],
+  [ "id" => 4, "admin" => "true" ]
+];
+
+foreach($users as $user){
+  if($user["admin"] == "true"){
+    print_r($user);
+  }
+};
