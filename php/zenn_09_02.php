b325944779907de36031389cdb37819e8e8427cc
Saoki11111 <t.aoki11111@gmail.com>
2021-01-07 23:13:16 +0900

    2回目

---
+++ php/zenn_09_02.php
@@ -0,0 +1,15 @@
+<?php
+# 次の連想配列の中からnameプロパティをもったユーザーに絞り込み、getNameという変数に格納して下さい。
+
+$users = [
+  [ id => 1, name => "豊臣" ],
+  [ id => 2 ],
+  [ id => 3, name => "織田" ],
+];
+
+foreach($users as $user){
+  if(array_key_exists("name", $user)){
+    print_r($user);
+  }
+};
+
